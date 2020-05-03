0797653575fc85d927ed2f04935e946e5f419bdd
cassiofb-dev <cassiofb.souza@gmail.com>
2020-05-02 23:15:13 -0300

    added gif command

---
+++ src/commands/gif.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const Giphy = require('../services/giphy.js');
+
+module.exports = {
+	name: 'gif',
+	description: 'Search a gif on Giphy',
+	cooldown: 3,
+	aliases: ['animation', 'giphy'],
+	async execute(message, args) {
+        const giphy_key = process.env.GIPHY_KEY;
+        const gif = await Giphy.gif(args.join(' '), giphy_key);
+		const embed = new Discord.MessageEmbed()
+			.setColor('#00aaff')
+			.setTitle('Here is your image!')
+			.addFields(
+				{ name: 'Author title', value: gif.title || 'No title 😢' },
+			)
+			.setImage(gif.url);
+		message.channel.send(embed);
+	},
+};
---
+++ src/services/giphy.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+const api_url = 'https://api.giphy.com/v1/gifs/search?';
+
+module.exports = {
+    gif: async (input, key) => {
+        const query = querystring.stringify({ api_key: key, q: input });
+        const received = await fetch(api_url + query).catch(e => console.log(e)).then(r => r.json());
+        const gifs = received.data;
+
+        const gif = gifs[Math.floor(Math.random() * gifs.length)];
+        return gif;
+    }
+};
